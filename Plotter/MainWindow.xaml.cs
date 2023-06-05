d4cd2c41ef7e42881e225a48dd57f5d663008a47
Johan Westerink <johanw9494@web.de>
2023-06-05 15:44:40 +0200

    Änderung Visu

--- Plotter/MainWindow.xaml.cs
+++ Plotter/MainWindow.xaml.cs
@@ -23,39 +23,23 @@ namespace Plotter
     /// 
     public partial class MainWindow : Window
     {
-        private string GcodeName = "";
-        private string Gcode = "";
-        GcodeHandler Test;
+        private Automatik AutoWindow = new Automatik();
+        private Hand HandWindow = new Hand();
         public MainWindow()
         {
             InitializeComponent();
-            Importbtn.Click += new RoutedEventHandler(this.Importbtn_Click);
-            Startbtn.Click += new RoutedEventHandler(this.Startbtn_Click);
+            BtnAuto.Click += new RoutedEventHandler(this.BtnAuto_Click);
+            BtnHand.Click += new RoutedEventHandler(this.BtnHand_Click);
         }
 
-        private  void Importbtn_Click(Object sender,EventArgs e)
+        private void BtnAuto_Click(Object sender, EventArgs e)
         {
-            OpenFileDialog GcodeFile = new OpenFileDialog();
-            if (GcodeFile.ShowDialog() == true)
-            {
-                this.GcodeName = GcodeFile.FileName;
-                this.Gcode = File.ReadAllText(this.GcodeName);
-                //GcodeText.Text = this.Gcode;
-            }
+            Main.Content = AutoWindow; ;
         }
-        private void Startbtn_Click(object sender,EventArgs e)
+
+        private void BtnHand_Click(Object sender, EventArgs e)
         {
-            if (true/*GcodeName.Contains(".gcode")*/)
-            {
-                Test = new GcodeHandler(GcodeName);
-                Test.Set_LinearSpeed(500);
-                Test.Set_StepResolution(360);
-                List<int[]> ttt = Test.Get_Steps();
-                foreach (var print in ttt)
-                {
-                    GcodeText.Text += print[0].ToString() + " " + print[1].ToString() + " " + print[2].ToString() + " " + print[3].ToString() + " " + print[4].ToString() + " " + print[5].ToString() + "\n";
-                }
-            }
+            Main.Content = HandWindow;
         }
     }
 }
---
+++ Plotter/Visu/Automatik.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Plotter
+{
+    /// <summary>
+    /// Interaktionslogik für Automatik.xaml
+    /// </summary>
+    public partial class Automatik : Page
+    {
+        private string GcodeName = "";
+        private string Gcode = "";
+        GcodeHandler Test;
+        public Automatik()
+        {
+            InitializeComponent();
+            Importbtn.Click += new RoutedEventHandler(this.Importbtn_Click);
+            Startbtn.Click += new RoutedEventHandler(this.Startbtn_Click);
+        }
+        private void Importbtn_Click(Object sender, EventArgs e)
+        {
+            OpenFileDialog GcodeFile = new OpenFileDialog();
+            if (GcodeFile.ShowDialog() == true)
+            {
+                this.GcodeName = GcodeFile.FileName;
+                this.Gcode = File.ReadAllText(this.GcodeName);
+                //GcodeText.Text = this.Gcode;
+            }
+        }
+        private void Startbtn_Click(object sender, EventArgs e)
+        {
+            if (true/*GcodeName.Contains(".gcode")*/)
+            {
+                Test = new GcodeHandler(GcodeName);
+                Test.Set_LinearSpeed(500);
+                Test.Set_StepResolution(360);
+                List<int[]> ttt = Test.Get_Steps();
+                foreach (var print in ttt)
+                {
+                    GcodeText.Text += print[0].ToString() + " " + print[1].ToString() + " " + print[2].ToString() + " " + print[3].ToString() + " " + print[4].ToString() + " " + print[5].ToString() + "\n";
+                }
+            }
+        }
+    }
+
+}
